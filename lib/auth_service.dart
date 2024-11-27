3ad706f6b2a8026b518f98b7b9f3b53330201527
yongchaeyoung <happy7yong83@gmail.com>
2024-11-27 14:55:56 +0900

    로그인 기능 추가

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -60,7 +60,30 @@ class AuthService extends ChangeNotifier {
     required Function() onSuccess, // 로그인 성공시 호출되는 함수
     required Function(String err) onError, // 에러 발생시 호출되는 함수
   }) async {
-    // 로그인
+    if (email.isEmpty) {
+      onError('이메일을 입력해주세요.');
+      return;
+    } else if (password.isEmpty) {
+      onError('비밀번호를 입력해주세요.');
+      return;
+    }
+
+    // 로그인 시도
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      onSuccess(); // 성공 함수 호출
+      notifyListeners(); // 로그인 상태 변경 알림
+    } on FirebaseAuthException catch (e) {
+      // firebase auth 에러 발생
+      onError(e.message!);
+    } catch (e) {
+      // Firebase auth 이외의 에러 발생
+      onError(e.toString());
+    }
   }
 
   void signOut() async {

e7150c7513554add0d6c5f16f382885ef79b7c24
yongchaeyoung <happy7yong83@gmail.com>
2024-11-27 11:26:32 +0900

    fit : 패키지 -> 해당파일에서 import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:moodo/auth_service.dart';
+import 'auth_service.dart';
 import 'package:provider/provider.dart';
 
 void main() {

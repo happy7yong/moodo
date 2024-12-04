ea5f75d3cb8fe67b778b641536d6a97931f2d5e0
yongchaeyoung <happy7yong83@gmail.com>
2024-12-05 03:56:38 +0900

    moodemoji상태업데이트 및 pytorch로부터 값을 불러오는 future코드 작성

--- lib/diaryPage.dart
+++ lib/diaryPage.dart
@@ -2,23 +2,57 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:moodo/auth_service.dart';
 import 'package:moodo/diary_service.dart';
+import 'package:moodo/moodSelector.dart';
+import 'package:moodo/sentiment_Analyzer.dart';
 import 'package:provider/provider.dart';
+import 'moodSelector.dart';
 
-class Diarypage extends StatelessWidget {
-  TextEditingController DiaryCollection = TextEditingController();
+class Diarypage extends StatefulWidget {
   final DateTime selectedDate;
-  bool isEditing = false; // 기록 수정 여부
-  String? selectedDocId;
 
-  Diarypage({
+  const Diarypage({
     super.key,
     required this.selectedDate,
   });
 
   @override
+  _DiarypageState createState() => _DiarypageState();
+}
+
+class _DiarypageState extends State<Diarypage> {
+  final TextEditingController DiaryCollection = TextEditingController();
+  final SentimentAnalyzer analyzer = SentimentAnalyzer();
+  String _selectedMood = 'default';
+  String sentiment = "";
+  String? selectedDocId;
+
+  // 감정과 이미지 파일 연결
+  final Map<String, String> moodImages = {
+    'positive': 'assets/images/moodEmoji/positiveMood.png',
+    'neutral': 'assets/images/moodEmoji/neutralityMood.png',
+    'negative': 'assets/images/moodEmoji/negativeMood.png',
+    'default': 'assets/images/moodEmoji/defaultMood.png',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    analyzer.loadModel();
+  }
+
+  Future<void> _analyzeSentiment() async {
+    String text = DiaryCollection.text;
+    String result = await analyzer.analyzeSentiment(text);
+    setState(() {
+      sentiment = result;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final authService = context.read<AuthService>();
     final user = authService.currentUser()!;
+
     return Consumer<DiaryService>(
       builder: (context, DiaryService, child) {
         return Scaffold(
@@ -26,18 +60,42 @@ class Diarypage extends StatelessWidget {
           appBar: AppBar(
             backgroundColor: const Color.fromRGBO(251, 250, 248, 1),
             leading: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.arrow_back_ios_new)),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios_new),
+            ),
           ),
           body: Center(
             child: Column(
               children: [
-                Image.asset(
-                  'assets/images/defaultMood.png',
-                  width: 90,
-                  height: 90,
+                GestureDetector(
+                  onTap: () {
+                    print('버튼이 클릭되었습니다!');
+
+                    showModalBottomSheet(
+                        backgroundColor: const Color.fromRGBO(251, 250, 248, 1),
+                        context: context,
+                        builder: (BuildContext context) {
+                          return MoodSelector(onMoodSelected: (moodEmoji) {
+                            setState(() {
+                              _selectedMood = moodEmoji; //선택한 이미지로 상태 업데이트
+                            });
+                            Navigator.pop(context); // 모달 닫기
+                          });
+                        });
+                  },
+                  child: Image.asset(
+                    moodImages[_selectedMood]!,
+                    width: 90,
+                    height: 90,
+                  ),
+                ),
+                Text(
+                  sentiment,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
                 const SizedBox(height: 10),
                 Container(
@@ -45,11 +103,13 @@ class Diarypage extends StatelessWidget {
                   height: 70,
                   padding: const EdgeInsets.all(6),
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                          color: const Color.fromRGBO(255, 221, 173, 1),
-                          width: 1),
-                      borderRadius: BorderRadius.circular(15)),
+                    color: Colors.white,
+                    border: Border.all(
+                      color: const Color.fromRGBO(255, 221, 173, 1),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                   child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -70,16 +130,13 @@ class Diarypage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                          "${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일",
-                          style: const TextStyle(fontSize: 18)),
-                      const SizedBox(
-                        width: 10,
+                        "${widget.selectedDate.year}년 ${widget.selectedDate.month}월 ${widget.selectedDate.day}일",
+                        style: const TextStyle(fontSize: 18),
                       ),
+                      const SizedBox(width: 10),
                       const Text(
                         '수요일',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                        style: TextStyle(fontSize: 18),
                       )
                     ],
                   ),
@@ -88,141 +145,132 @@ class Diarypage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
                     child: FutureBuilder<QuerySnapshot>(
-                        future: DiaryService.read(user.uid),
-                        builder: (context, snapshot) {
-                          final documents =
-                              snapshot.data?.docs ?? []; //문서들 가져오기
-                          String initialText = '';
-
-                          // 문서에서 오늘의 날짜와 일치하는 데이터 가져오기
-                          final todayString =
-                              "${selectedDate.year}-${selectedDate.month}-${selectedDate.day}";
-
-                          for (var doc in documents) {
-                            final data = doc.data() as Map<String, dynamic>;
-                            if (data['date'] == todayString) {
-                              initialText = data['content'] ?? '';
-                              break;
-                            }
+                      future: DiaryService.read(user.uid),
+                      builder: (context, snapshot) {
+                        final documents = snapshot.data?.docs ?? [];
+                        String initialText = '';
+                        bool isExistingEntry = false;
+                        final todayString =
+                            "${widget.selectedDate.year}-${widget.selectedDate.month}-${widget.selectedDate.day}";
+                        for (var doc in documents) {
+                          final data = doc.data() as Map<String, dynamic>;
+                          if (data['date'] == todayString) {
+                            initialText = data['content'] ?? '';
+                            isExistingEntry = true;
+                            break;
                           }
-
-                          // 컨트롤러 값 설정
-                          DiaryCollection.text = initialText;
-
-                          return TextField(
-                            controller: DiaryCollection,
-                            decoration: const InputDecoration(
-                                hintText: "오늘은 어떤 하루를 보내셨나요?",
-                                hintStyle: TextStyle(
-                                    color: Color.fromRGBO(212, 212, 212, 1),
-                                    fontSize: 18),
-                                border: InputBorder.none),
-                            maxLines: null,
-                          );
-                        }),
+                        }
+                        DiaryCollection.text = initialText;
+                        return TextField(
+                          controller: DiaryCollection,
+                          decoration: const InputDecoration(
+                            hintText: "오늘은 어떤 하루를 보내셨나요?",
+                            hintStyle: TextStyle(
+                              color: Color.fromRGBO(212, 212, 212, 1),
+                              fontSize: 18,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                          maxLines: null,
+                        );
+                      },
+                    ),
                   ),
                 ),
                 ElevatedButton(
-                    onPressed: () {
-                      final formattedDate =
-                          "${selectedDate.year}-${selectedDate.month}-${selectedDate.day}";
-                      if (isEditing) {
-                        DiaryService.update(selectedDocId!, formattedDate,
-                            user.uid, DiaryCollection.text);
-                      } else {
-                        DiaryService.create(
-                            formattedDate, user.uid, DiaryCollection.text);
-                      }
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            backgroundColor: Colors.white,
-                            content: SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.8,
-                              height: 250,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const SizedBox(
-                                    height: 14,
+                  onPressed: () async {
+                    final formattedDate =
+                        "${widget.selectedDate.year}-${widget.selectedDate.month}-${widget.selectedDate.day}";
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          backgroundColor: Colors.white,
+                          content: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: 250,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const SizedBox(height: 14),
+                                Image.asset(
+                                  'assets/images/diaryIcon.png',
+                                  width: 130,
+                                  height: 130,
+                                ),
+                                const SizedBox(height: 20),
+                                const Text(
+                                  '오늘의 기록을 모두 작성하셨나요?',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                const SizedBox(height: 13),
+                                const SizedBox(
+                                  width: 300,
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        '현재 작성된 일기를 기반으로',
+                                        style:
+                                            TextStyle(fontSize: 17, height: 1),
+                                      ),
+                                      Text(
+                                        '감정 이모티콘을 추천해드릴게요!',
+                                        style:
+                                            TextStyle(fontSize: 17, height: 1),
+                                      )
+                                    ],
                                   ),
-                                  Image.asset(
-                                    'assets/images/diaryIcon.png',
-                                    width: 130,
-                                    height: 130,
-                                  ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  const Text(
-                                    '오늘의 기록을 모두 작성하셨나요?',
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  const SizedBox(
-                                    height: 13,
-                                  ),
-                                  const SizedBox(
-                                    width: 300,
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          '현재 작성된 일기를 기반으로',
-                                          style: TextStyle(
-                                              fontSize: 17, height: 1),
-                                        ),
-                                        Text(
-                                          '감정 이모티콘을 추천해드릴게요!',
-                                          style: TextStyle(
-                                              fontSize: 17, height: 1),
-                                        )
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
+                                )
+                              ],
                             ),
-                            actions: [
-                              Center(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    final formattedDate =
-                                        "${selectedDate.year}-${selectedDate.month}-${selectedDate.day}";
-                                    Navigator.of(context).pop();
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: const Color.fromRGBO(
-                                          255, 186, 101, 1),
-                                      elevation: 0,
-                                      minimumSize: const Size(140, 50)),
-                                  child: const Text(
-                                    '감정 받기',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18),
-                                  ),
+                          ),
+                          actions: [
+                            Center(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  _analyzeSentiment;
+                                  final formattedDate =
+                                      "${widget.selectedDate.year}-${widget.selectedDate.month}-${widget.selectedDate.day}";
+                                  DiaryService.create(formattedDate, user.uid,
+                                      DiaryCollection.text);
+
+                                  Navigator.of(context).pop();
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromRGBO(255, 186, 101, 1),
+                                  elevation: 0,
+                                  minimumSize: const Size(140, 50),
+                                ),
+                                child: const Text(
+                                  '감정 받기',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
                                 ),
                               ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(53, 47, 47, 1),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        minimumSize: const Size(120, 50),
-                        elevation: 0),
-                    child: const Text(
-                      '기록 완료',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 19,
-                      ),
-                    )),
-                const SizedBox(
-                  height: 20,
-                )
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(53, 47, 47, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    minimumSize: const Size(120, 50),
+                    elevation: 0,
+                  ),
+                  child: const Text(
+                    '기록 완료',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20)
               ],
             ),
           ),

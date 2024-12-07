95adf92d7d3201b870bc4a6380229fe66fe9eba3
yongchaeyoung <happy7yong83@gmail.com>
2024-12-07 15:14:33 +0900

    꽃 방 페이지 : 기본적 템플릿 구조 생성

--- lib/flowerRoom.dart
+++ lib/flowerRoom.dart
@@ -1,1 +1,26 @@
-class 
+import 'package:flutter/material.dart';
+
+class FlowerRoom extends StatelessWidget {
+  const FlowerRoom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //final authService = context.read<AuthService>();
+    //final user = authService.currentUser()!;
+    Color:
+    Color.fromRGBO(251, 250, 248, 1);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Text('11월의 꽃은', style: TextStyle()),
+        Image.asset('', width: 100, height: 100),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+}

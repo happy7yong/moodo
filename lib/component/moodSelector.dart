26fff703e1a3a73917a7443bf26e40929a5ab580
yongchaeyoung <happy7yong83@gmail.com>
2024-12-08 14:26:57 +0900

    감정 수정 완료

--- lib/component/moodSelector.dart
+++ lib/component/moodSelector.dart
@@ -1,13 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moodo/auth_service.dart';
+import 'package:moodo/diary_service.dart';
+import 'package:provider/provider.dart';
 
-class MoodSelector extends StatelessWidget {
+class MoodSelector extends StatefulWidget {
   final Function(String) onMoodSelected;
+  final String currentDocId; // 현재 문서 ID를 추가로 전달받음
 
-  const MoodSelector({super.key, required this.onMoodSelected});
+  const MoodSelector(
+      {super.key, required this.onMoodSelected, required this.currentDocId});
 
   @override
+  _MoodSelectorState createState() => _MoodSelectorState();
+}
+
+class _MoodSelectorState extends State<MoodSelector> {
+  @override
   Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
+    final user = authService.currentUser()!;
     return SizedBox(
       height: 200,
       child: Center(
@@ -15,21 +28,36 @@ class MoodSelector extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
+              SizedBox(
+                height: 10,
+              ),
               Container(
                 color: const Color.fromRGBO(219, 219, 219, 1),
                 height: 4,
                 width: 40,
               ),
+              SizedBox(
+                height: 10,
+              ),
               const Text(
                 '오늘 하루의 감정을 입혀볼까요?',
-                style: TextStyle(fontSize: 17),
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 15,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   GestureDetector(
-                    onTap: () {
-                      onMoodSelected('positive'); // 감정명 전달
+                    onTap: () async {
+                      await FirebaseFirestore.instance
+                          .collection('Diary')
+                          .doc(widget.currentDocId) // 전달된 문서 ID 사용
+                          .update({'mood': 'positive'});
+                      widget.onMoodSelected('positive');
                     },
                     child: Image.asset(
                       'assets/images/moodEmoji/positiveMood.png',
@@ -38,8 +66,12 @@ class MoodSelector extends StatelessWidget {
                     ),
                   ),
                   GestureDetector(
-                    onTap: () {
-                      onMoodSelected('neutral');
+                    onTap: () async {
+                      await FirebaseFirestore.instance
+                          .collection('Diary')
+                          .doc(widget.currentDocId) // 전달된 문서 ID 사용
+                          .update({'mood': 'neutral'});
+                      widget.onMoodSelected('neutral');
                     },
                     child: Image.asset(
                       'assets/images/moodEmoji/neutralityMood.png',
@@ -48,8 +80,12 @@ class MoodSelector extends StatelessWidget {
                     ),
                   ),
                   GestureDetector(
-                    onTap: () {
-                      onMoodSelected('negative');
+                    onTap: () async {
+                      await FirebaseFirestore.instance
+                          .collection('Diary')
+                          .doc(widget.currentDocId) // 전달된 문서 ID 사용
+                          .update({'mood': 'negative'});
+                      widget.onMoodSelected('negative');
                     },
                     child: Image.asset(
                       'assets/images/moodEmoji/negativeMood.png',

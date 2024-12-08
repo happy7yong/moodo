19df826b84c5ddd3f9bb30fdaa29094311cce0ad
yongchaeyoung <happy7yong83@gmail.com>
2024-12-09 06:11:58 +0900

    현재페이지에 대한 즉각적인 반응 및 selectedMonth별 꽃 반응

--- lib/component/flowerPotImage.dart
+++ lib/component/flowerPotImage.dart
@@ -16,52 +16,51 @@ class _FlowerpotImageState extends State<FlowerpotImage> {
   static const String step_3 = 'assets/images/pot/pot3.png';
   static const String step_4 = 'assets/images/pot/pot4.png';
 
-  String potImage = Default_pot;
-  int dataCount = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    checkDataCount();
-  }
-
-  Future<void> checkDataCount() async {
-    final currentMonth = widget.selectedMonth.toString().padLeft(2, '0');
-    final currentTime = DateTime.now();
-    final querySnapshot = await FirebaseFirestore.instance
-        .collection('Diary')
-        .where('date',
-            isLessThan:
-                '${currentTime.year}-${(widget.selectedMonth + 1).toString().padLeft(2, '0')}-01')
-        .get();
-
-    setState(() {
-      dataCount = querySnapshot.docs.length;
-
-      if (dataCount >= 5) {
-        potImage = step_2;
-      } else if (dataCount >= 10) {
-        potImage = step_3;
-      } else if (dataCount >= 18) {
-        potImage = step_4;
-      } else {
-        potImage = Default_pot;
-      }
-    });
+  String getPotImage(int dataCount) {
+    if (dataCount >= 18) {
+      return step_4;
+    } else if (dataCount >= 10) {
+      return step_3;
+    } else if (dataCount >= 5) {
+      return step_2;
+    } else {
+      return Default_pot;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print("화분이 클릭되었습니다!");
-        print('현재 월(${widget.selectedMonth})의 데이터 개수: $dataCount');
+    final currentTime = DateTime.now();
+    final selectedMonth = widget.selectedMonth.toString().padLeft(2, '0');
+    final nextMonth = (widget.selectedMonth + 1).toString().padLeft(2, '0');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('Diary')
+          .where('date',
+              isGreaterThanOrEqualTo: '${currentTime.year}-$selectedMonth-01')
+          .where('date', isLessThan: '${currentTime.year}-$nextMonth-01')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        final dataCount = snapshot.data?.docs.length ?? 0;
+        final potImage = getPotImage(dataCount);
+
+        return GestureDetector(
+          onTap: () {
+            print("화분이 클릭되었습니다!");
+            print('현재 월(${widget.selectedMonth})의 데이터 개수: $dataCount');
+          },
+          child: Image.asset(
+            potImage,
+            width: 100,
+            height: 100,
+          ),
+        );
       },
-      child: Image.asset(
-        potImage,
-        width: 100,
-        height: 100,
-      ),
     );
   }
 }

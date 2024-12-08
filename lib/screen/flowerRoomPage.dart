6110072a0244d3761e913b5b5425801967092b12
yongchaeyoung <happy7yong83@gmail.com>
2024-12-09 08:24:10 +0900

    mood 비율에 따른 꽃 종류 및 이름 로직 추가

--- lib/screen/flowerRoomPage.dart
+++ lib/screen/flowerRoomPage.dart
@@ -1,24 +1,116 @@
 import 'package:flutter/material.dart';
 
-class FlowerRoom extends StatelessWidget {
-  const FlowerRoom({super.key});
+class Flowerroompage extends StatelessWidget {
+  final Map<String, int> moodStats;
+  final int month;
+
+  const Flowerroompage({
+    super.key,
+    required this.moodStats,
+    required this.month,
+  });
 
   @override
   Widget build(BuildContext context) {
-    //final authService = context.read<AuthService>();
-    //final user = authService.currentUser()!;
-    Color:
-    Color.fromRGBO(251, 250, 248, 1);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: 30,
+    final String flowerName;
+    final String flowerImage;
+    //mood count
+    final positiveCount = moodStats['positive'] ?? 0;
+    final neutralCount = moodStats['neutral'] ?? 0;
+    final negativeCount = moodStats['negative'] ?? 0;
+
+    //mood 비율에 따른 꽃 종류
+    if (positiveCount > neutralCount && neutralCount > negativeCount) {
+      flowerImage = 'assets/images/flower/flowerRose.png';
+      flowerName = '장미꽃';
+    } else if (positiveCount < neutralCount && neutralCount < negativeCount) {
+      flowerImage = 'assets/images/flower/flowerSilver.png';
+      flowerName = '은방울꽃';
+    } else {
+      flowerImage = 'assets/images/flower/defaultFlower.png';
+      flowerName = '기본 꽃';
+    }
+
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(251, 250, 248, 1),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(251, 250, 248, 1),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    '$month월',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    '당신의 꽃은,',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Image.asset(
+                    flowerImage,
+                    width: 400,
+                    height: 400,
+                  ),
+                  Text(
+                    '$flowerName이 피어났어요',
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildMoodStatCard(
+                  'Positive',
+                  moodStats['positive'] ?? 0,
+                  Colors.red,
+                ),
+                _buildMoodStatCard(
+                  'Neutral',
+                  moodStats['neutral'] ?? 0,
+                  Colors.yellow,
+                ),
+                _buildMoodStatCard(
+                  'Negative',
+                  moodStats['negative'] ?? 0,
+                  Colors.blue,
+                ),
+              ],
+            ),
+          ],
         ),
-        Text('11월의 꽃은', style: TextStyle()),
-        Image.asset('', width: 100, height: 100),
-        SizedBox(
-          height: 30,
+      ),
+    );
+  }
+
+  Widget _buildMoodStatCard(String mood, int count, Color color) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: color,
+          child: Text(
+            count.toString(),
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          mood,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
       ],
     );

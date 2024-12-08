b257c0f802b1095062d742402203504a52912224
yongchaeyoung <happy7yong83@gmail.com>
2024-12-09 08:23:41 +0900

    moodscore 추가

--- lib/component/flowerPotImage.dart
+++ lib/component/flowerPotImage.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:moodo/screen/flowerRoomPage.dart';
 
 class FlowerpotImage extends StatefulWidget {
   final int selectedMonth;
@@ -28,31 +29,70 @@ class _FlowerpotImageState extends State<FlowerpotImage> {
     }
   }
 
+  Future<Map<String, int>> getMoodStatistics() async {
+    final currentTime = DateTime.now();
+    final nextMonth = (widget.selectedMonth + 1).toString().padLeft(2, '0');
+    final selectedMonth = widget.selectedMonth.toString().padLeft(2, '0');
+
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('Diary')
+        .where('date',
+            isGreaterThanOrEqualTo: '${currentTime.year}-$selectedMonth-01')
+        .where('date', isLessThan: '${currentTime.year}-$nextMonth-01')
+        .get();
+
+    final moodCounts = {
+      'positive': 0,
+      'neutral': 0,
+      'negative': 0,
+    };
+
+    for (var doc in querySnapshot.docs) {
+      final mood = doc['mood'] ?? 'default';
+      if (moodCounts.containsKey(mood)) {
+        moodCounts[mood] = moodCounts[mood]! + 1;
+      }
+    }
+
+    return moodCounts;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentTime = DateTime.now();
-    final selectedMonth = widget.selectedMonth.toString().padLeft(2, '0');
-    final nextMonth = (widget.selectedMonth + 1).toString().padLeft(2, '0');
+    final now = DateTime.now();
+    //월의 마지막일자
+    final isEndOfMonth = DateTime(now.year, now.month, now.day)
+        .isAfter(DateTime(now.year, widget.selectedMonth, 28)); // 28일 이후 조건
 
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance
           .collection('Diary')
           .where('date',
-              isGreaterThanOrEqualTo: '${currentTime.year}-$selectedMonth-01')
-          .where('date', isLessThan: '${currentTime.year}-$nextMonth-01')
+              isGreaterThanOrEqualTo: '${now.year}-${widget.selectedMonth}-01')
+          .where('date',
+              isLessThan: '${now.year}-${widget.selectedMonth + 1}-01')
           .snapshots(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        }
-
         final dataCount = snapshot.data?.docs.length ?? 0;
         final potImage = getPotImage(dataCount);
 
         return GestureDetector(
-          onTap: () {
-            print("화분이 클릭되었습니다!");
-            print('현재 월(${widget.selectedMonth})의 데이터 개수: $dataCount');
+          onTap: () async {
+            if (isEndOfMonth) {
+              final moodStats = await getMoodStatistics();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Flowerroompage(
+                    moodStats: moodStats,
+                    month: widget.selectedMonth,
+                  ),
+                ),
+              );
+            } else {
+              print("화분이 클릭되었습니다!");
+              print('현재 월(${widget.selectedMonth})의 데이터 개수: $dataCount');
+            }
           },
           child: Image.asset(
             potImage,
